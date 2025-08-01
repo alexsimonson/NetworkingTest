719073553c1d11f33aeee9a639961b1e657ef221
Alex <alexsimonson@knights.ucf.edu>
2025-08-01 10:33:03 -0400

    proper deletion of fireballs on server

--- Assets/Scripts/ProjectileMovement.cs
+++ Assets/Scripts/ProjectileMovement.cs
@@ -1,17 +1,31 @@
 using UnityEngine;
+using Unity.Netcode;
 
-public class ProjectileMovement : MonoBehaviour
+public class ProjectileMovement : NetworkBehaviour
 {
 
     public float projectileSpeed = 100f;
+    public float projectileLife = 10f;  // seconds
 
-    void Start(){
-        Destroy(gameObject, 10f);
+    void Start()
+    {
+        if (IsServer)
+        {
+            Invoke(nameof(DestroySelf), projectileLife);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         GetComponent<Rigidbody>().linearVelocity = transform.forward * projectileSpeed;
+    }
+
+    void DestroySelf()
+    {
+        if (NetworkObject != null && NetworkObject.IsSpawned)
+        {
+            NetworkObject.Despawn();
+        }
     }
 }
