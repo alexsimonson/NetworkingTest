5d8383634abc02f38d7544010110a53603a0d124
Alex <alexsimonson@knights.ucf.edu>
2025-08-02 21:36:50 -0400

    networked gun as ranged attack, fire direction based on look rotation.  also ported mage attack to utilize look rotation

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -4,21 +4,30 @@ using System.Collections;
 
 public class PlayerCombat : NetworkBehaviour
 {
-    public GameObject slashVFXPrefab;  // Assign in the inspector
+    public GameObject slashVFXPrefab;
     public GameObject fireballPrefab;
+    public GameObject muzzleFlashPrefab;
+    public GameObject hitEffectPrefab;
+
     private Transform projectileSpawnPoint;
-    
     private string[] debugWeaponsArray = new string[10];
     private int weaponIndex;
 
     public float meleeRange = 2f;
     public float meleeRadius = 1f;
     public int meleeDamage = 10;
+
+    public float gunRange = 50f;
+    public int gunDamage = 35;
     public LayerMask hitLayers;
 
-    void Awake(){
+    public GameObject playerCam;
+
+    void Awake()
+    {
         hitLayers = LayerMask.GetMask("Attackable");
         projectileSpawnPoint = transform.Find("ProjectileSpawn");
+        playerCam = transform.Find("Camera").gameObject;
     }
 
     void Start()
@@ -32,39 +41,89 @@ public class PlayerCombat : NetworkBehaviour
     {
         if (!IsOwner) return;
 
-        // Weapon selection
         if (Input.GetKeyDown(KeyCode.Alpha1)) weaponIndex = 1;
         else if (Input.GetKeyDown(KeyCode.Alpha2)) weaponIndex = 2;
         else if (Input.GetKeyDown(KeyCode.Alpha3)) weaponIndex = 3;
 
-        // Left-click attack (only if melee weapon is selected)
         if (Input.GetMouseButtonDown(0))
         {
-            switch(debugWeaponsArray[weaponIndex]){
+            switch (debugWeaponsArray[weaponIndex])
+            {
                 case "melee":
                     PerformMeleeAttackServerRpc();
                     break;
+                case "range":
+                    PerformGunAttackServerRpc(playerCam.transform.forward.normalized);
+                    break;
                 case "magic":
-                    PerformMagicAttackServerRpc();
+                    PerformMagicAttackServerRpc(Quaternion.LookRotation(playerCam.transform.forward.normalized));
                     break;
             }
         }
     }
 
     [ServerRpc]
-    void PerformMagicAttackServerRpc(ServerRpcParams rpcParams = default){
-        var rotation = Quaternion.LookRotation(transform.forward);
-
+    void PerformMagicAttackServerRpc(Quaternion rotation, ServerRpcParams rpcParams = default)
+    {
         var fireball = Instantiate(fireballPrefab, projectileSpawnPoint.position, rotation);
         fireball.GetComponent<ProjectileMovement>().owner = gameObject;
         fireball.GetComponent<NetworkObject>().Spawn();
+    }
+
+
+    [ServerRpc]
+    void PerformGunAttackServerRpc(Vector3 direction, ServerRpcParams rpcParams = default)
+    {
+        Vector3 origin = projectileSpawnPoint.position;
+
+        if (muzzleFlashPrefab)
+            SpawnEffectClientRpc(origin, direction, muzzleFlashPrefab.name);
+
+        ShowRayClientRpc(origin, direction, gunRange);
+
+        if (Physics.Raycast(origin, direction, out RaycastHit hit, gunRange, hitLayers))
+        {
+            if (hitEffectPrefab)
+                SpawnEffectClientRpc(hit.point, hit.normal, hitEffectPrefab.name);
+
+            if (hit.collider.TryGetComponent<Stats>(out var damageable))
+            {
+                damageable.TakeDamage(gunDamage);
+            }
+        }
+    }
+
+
+    [ClientRpc]
+    void ShowRayClientRpc(Vector3 origin, Vector3 direction, float distance)
+    {
+        GameObject lineObj = new GameObject("DebugRay");
+        var lr = lineObj.AddComponent<LineRenderer>();
+        lr.positionCount = 2;
+        lr.SetPosition(0, origin);
+        lr.SetPosition(1, origin + direction * distance);
+        lr.material = new Material(Shader.Find("Sprites/Default"));
+        lr.startColor = lr.endColor = Color.red;
+        lr.startWidth = lr.endWidth = 0.05f;
+        Destroy(lineObj, 1f);
+    }
+
+
+    [ClientRpc]
+    void SpawnEffectClientRpc(Vector3 pos, Vector3 normal, string effectName)
+    {
+        var prefab = Resources.Load<GameObject>(effectName);
+        if (!prefab) return;
+
+        var effect = Instantiate(prefab, pos, Quaternion.LookRotation(normal));
+        Destroy(effect, 2f);
     }
 
     [ServerRpc]
     void PerformMeleeAttackServerRpc(ServerRpcParams rpcParams = default)
     {
         StartCoroutine(SlashArcCoroutine());
-        PerformSlashVisualClientRpc();  // show effect on all clients
+        PerformSlashVisualClientRpc();
     }
 
     [ClientRpc]
@@ -73,49 +132,38 @@ public class PlayerCombat : NetworkBehaviour
         if (slashVFXPrefab)
         {
             GameObject vfx = Instantiate(slashVFXPrefab, transform.position, transform.rotation);
-            var ps = vfx.GetComponent<ParticleSystem>();
-            if (ps != null)
-            {
-                ps.Play();
-            }
-            else
-            {
-                Debug.LogWarning("No ParticleSystem found on VFX prefab.");
-            }
-
-            Destroy(vfx, 2f);  // Destroy after duration
+            if (vfx.TryGetComponent<ParticleSystem>(out var ps)) ps.Play();
+            Destroy(vfx, 2f);
         }
     }
 
     IEnumerator SlashArcCoroutine()
     {
-        // Vector3 origin = transform.position + Vector3.up * 1.0f;
         Vector3 origin = transform.position;
         Vector3 forward = transform.forward;
         int rayCount = 15;
         float slashArc = 120f;
-        float delayBetweenRays = 0.01f; // seconds
+        float delayBetweenRays = 0.01f;
         float maxDistance = meleeRange;
 
         Quaternion baseRotation = Quaternion.LookRotation(forward);
-
         bool alreadyDealtDamage = false;
 
         for (int i = 0; i < rayCount; i++)
         {
             float lerp = (float)i / (rayCount - 1);
-            float angle = Mathf.Lerp(slashArc / 2f, -slashArc / 2f, lerp); // right to left
+            float angle = Mathf.Lerp(slashArc / 2f, -slashArc / 2f, lerp);
             Quaternion rotation = baseRotation * Quaternion.Euler(0, angle, 0);
             Vector3 rayDirection = rotation * Vector3.forward;
 
-            // Debug draw
             Debug.DrawRay(origin, rayDirection * maxDistance, Color.red, 1f);
 
             if (Physics.Raycast(origin, rayDirection, out RaycastHit hit, maxDistance, hitLayers))
             {
                 if (hit.collider.TryGetComponent<Stats>(out var damageable))
                 {
-                    if(!alreadyDealtDamage){
+                    if (!alreadyDealtDamage)
+                    {
                         damageable.TakeDamage(meleeDamage);
                         alreadyDealtDamage = true;
                     }
